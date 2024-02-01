8f79f14fb7984132b0227ce032ce1276e30acc7d
Jinyao <jinyaox@uci.edu>
2024-01-31 16:22:45 -0800

    Fixed Python error bug

---
+++ Py_script/ectf_params_AP.h
@@ -0,0 +1,8 @@
+#ifndef __ECTF_PARAMS__
+#define __ECTF_PARAMS__
+#define AP_PIN "123456"
+#define AP_TOKEN "TOKEN123456789"
+#define COMPONENT_IDS 0x123444 0x123455
+#define COMPONENT_CNT 2
+#define AP_BOOT_MSG "Message"
+#endif
