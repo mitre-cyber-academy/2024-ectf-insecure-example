53190db150720fa4555c9ac553af7681dd53e3b3
Yintong Luo <yintol1@uci.edu>
2024-02-05 10:26:48 -0800

    added the new functions in the header file and corrected types

--- component/src/board_link.c
+++ component/src/board_link.c
@@ -104,7 +104,7 @@ int timed_wait_and_receive_packet(uint8_t* packet) {
  * This function utilizes the simple_i2c_peripheral library to
  * send a packet to the AP and wait for the message to be received
 */
-int secure_send_packet_and_ack(uint8_t len, uint8_t* packet, uint8_t* GLOBAL_KEY) {
+void secure_send_packet_and_ack(uint8_t len, uint8_t* packet, uint8_t* GLOBAL_KEY) {
     encrypt_sym(packet, len, GLOBAL_KEY, ciphertext);
     send_packet_and_ack(len, ciphertext);
 }
@@ -118,9 +118,9 @@ int secure_send_packet_and_ack(uint8_t len, uint8_t* packet, uint8_t* GLOBAL_KEY
  * This function waits for a new message to be available from the AP,
  * once the message is available it is returned in the buffer pointer to by packet 
 */
-int secure_wait_and_receive_packet(uint8_t* packet, uint8_t* GLOBAL_KEY) {
+uint8_t secure_wait_and_receive_packet(uint8_t* packet, uint8_t* GLOBAL_KEY) {
     uint8_t plaintext[MAX_I2C_MESSAGE_LEN];
-    int len = wait_and_receive_packet(packet);
+    uint8_t len = wait_and_receive_packet(packet);
     decrypt_sym(packet, MAX_I2C_MESSAGE_LEN, GLOBAL_KEY, plaintext);
     memmove(packet, plaintext, MAX_I2C_MESSAGE_LEN);
     return len;
